2bf11025e31d45a3ba524cd230be6021de2ce030
Corey <simon8888xsimon8888x@gmail.com>
2020-10-20 17:50:45 +0800

    test

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const exphbs = require('express-handlebars')
+const { static } = require('express')
+
+
+app.engine('hbs', exphbs({ defaultLayout: "main",extname: "hbs" }))
+app.set('view engine', 'hbs')
+app.use(express.static('public'))
+
+app.use('/', (req, res) => {
+  res.render('test')
+})
+
+app.listen(3000, () => {
+  // db.sequelize.sync()
+  console.log(`Example app listening on port !`)
+})
+console.log('test')
